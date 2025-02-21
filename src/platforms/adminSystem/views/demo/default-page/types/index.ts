e86da3598717d54f250ab29603c8c47930bc7f47
spidergod <spidergod23@gmail.com>
2025-02-21 16:20:40 +0800

    Create index.ts

---
+++ src/platforms/adminSystem/views/demo/default-page/types/index.ts
@@ -0,0 +1,34 @@
+/** index.vue **/
+export enum QueryType {
+  Search = "search",
+  Reset = "reset",
+}
+
+export interface ChoiceBaseItem {
+  id: string | number;
+  name: string;
+}
+
+// 這裡是Choices根據需求修改添加
+export type Choices = {
+  status: ChoiceBaseItem[];
+  product: ChoiceBaseItem[];
+  group: ChoiceBaseItem[];
+  user: ChoiceBaseItem[];
+  userDic: Record<string, string>;
+};
+// 這裡是搜尋條件根據需求修改添加
+export interface QueryParams extends PageQuery {
+  keywords: string;
+  group: number | null;
+  dept: number | null;
+  status?: number;
+}
+/** edit.vue **/
+export type ShowParams<TForm> =
+  | { type: "ADD"; editForm: {} }
+  | { type: "EDIT"; editForm: Partial<TForm> };
+
+export type SubmitParams<TForm> =
+  | { type: "ADD"; data: TForm }
+  | { type: "EDIT"; data: TForm };
