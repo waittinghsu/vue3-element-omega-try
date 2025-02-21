1e1cd886423f344435424d27c3e94b7b568ff24a
spidergod <spidergod23@gmail.com>
2025-02-21 16:21:23 +0800

    Create columns.ts

---
+++ src/platforms/adminSystem/views/demo/default-page/config/columns.ts
@@ -0,0 +1,9 @@
+import { TableColumnCtx } from "element-plus";
+
+// export const columns: TableColumnCtx<any>[] = [
+//   {
+//     label: "Column Label",
+//     prop: "columnProp",
+//     width: 120 as any, // Width can be a string or a number
+//   },
+// ];
